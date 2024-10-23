85141c324623cf5b8d86212664a3ab4e18f3dad7
Rehanga Gamage <RehangaG@brownsgroup.com>
2024-10-23 11:35:35 +0530

    Converted to a timer trigger (Azure functions)

--- function_app.py
+++ function_app.py
@@ -1,32 +1,46 @@
-# 21:32
+# import azure.functions as func
+# import logging
+# from main_manual import run_main
+
+# app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+# @app.route(route="harti1http")
+# def harti1http(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Harti Page 1 ETL triggered.')
+
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
+
+#     run_main()
+
+#     # if name:
+#     #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+#     # else:
+#     #     return func.HttpResponse(
+#     #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+#     #          status_code=200
+#     #     )
+
+    
+#     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+
 import azure.functions as func
 import logging
 from main_manual import run_main
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="harti1http")
-def harti1http(req: func.HttpRequest) -> func.HttpResponse:
+# @app.schedule(schedule="0 */10 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+@app.schedule(schedule="0 0 16 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+def harti_etl_page1(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
     logging.info('Harti Page 1 ETL triggered.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
     run_main()
-
-    # if name:
-    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-    #     return func.HttpResponse(
-    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-    #          status_code=200
-    #     )
-
-    
-    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
